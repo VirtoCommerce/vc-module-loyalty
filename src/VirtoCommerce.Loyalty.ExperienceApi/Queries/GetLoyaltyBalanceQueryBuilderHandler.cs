fdbca65fc687932d1d7f61678b01a6b8512af034
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-09-09 15:03:46 +0200

    fix: sonar

--- src/VirtoCommerce.Loyalty.ExperienceApi/Queries/GetLoyaltyBalanceQueryBuilderHandler.cs
+++ src/VirtoCommerce.Loyalty.ExperienceApi/Queries/GetLoyaltyBalanceQueryBuilderHandler.cs
@@ -2,7 +2,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using VirtoCommerce.Loyalty.Core.Models;
 using VirtoCommerce.Loyalty.Core.Services;
-using VirtoCommerce.OrdersModule.Core.Services;
 using VirtoCommerce.Xapi.Core.Infrastructure;
 
 namespace VirtoCommerce.Loyalty.ExperienceApi.Queries;
@@ -10,12 +9,10 @@ namespace VirtoCommerce.Loyalty.ExperienceApi.Queries;
 public class GetLoyaltyBalanceQueryBuilderHandler : IQueryHandler<GetLoyaltyBalanceQuery, LoyaltyBalanceResult>
 {
     private readonly ILoyaltyLogicService _loyaltyLogicService;
-    private readonly ICustomerOrderService _customerOrderService;
 
-    public GetLoyaltyBalanceQueryBuilderHandler(ILoyaltyLogicService loyaltyLogicService, ICustomerOrderService customerOrderService)
+    public GetLoyaltyBalanceQueryBuilderHandler(ILoyaltyLogicService loyaltyLogicService,)
     {
         _loyaltyLogicService = loyaltyLogicService;
-        _customerOrderService = customerOrderService;
     }
 
     public async Task<LoyaltyBalanceResult> Handle(GetLoyaltyBalanceQuery request, CancellationToken cancellationToken)
